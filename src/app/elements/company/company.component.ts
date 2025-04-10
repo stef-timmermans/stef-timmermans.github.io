28f5bae903ac55405154149fd4a540c7068b81c4
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-04-10 16:26:02 +0200

    propagate down company information into list items

--- src/app/elements/company/company.component.ts
+++ src/app/elements/company/company.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+/*
+* Folder: src/app/elements/company
+* Component: company
+*     This component is used to display a company. It is
+*   called by the resume component's HTML and is passed the
+*   company's information. These fields are the project's ID
+*   number, name, role, description, tags, and LinkedIn page.
+*/
+
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-company',
@@ -6,5 +15,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./company.component.scss']
 })
 export class CompanyComponent {
+
+  @Input() id!: number;
+  @Input() name!: string;
+  @Input() role!: string;
+  @Input() description!: string;
+  @Input() tags!: string[];
+  @Input() linkedin!: string;
 
 }
