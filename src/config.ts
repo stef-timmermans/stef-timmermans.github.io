42dc6b59a60da718e0737d0c12a0f9f1304e3720
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-04-14 13:20:43 +0200

    fix: resume link to be more secure

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
     // URL to resume source
     // TODO: READ ONLY FOLDER!!! This needs to become a secret passed into /docs in build step...
-    resumeUrl: 'https://drive.google.com/drive/folders/1b28aCtSnSwO3qvwIdeL11sUe1V9ZKulI?usp=sharing'
+    resumeUrl: 'https://drive.google.com/file/d/1a4mNfhP5hucGwa8-HAxL4h2NI-kU3p99/view?usp=share_link'
 }
